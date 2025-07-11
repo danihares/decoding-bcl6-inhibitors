72893446222e31283ef6cc1ff9042a07334c37bb
danihares <107181549+danihares@users.noreply.github.com>
2025-07-11 10:37:09 +0100

    Add files via upload

---
+++ run1-bnd.cmd
@@ -0,0 +1,37 @@
+parfile $PROTOMSHOME/parameter/amber14SB.ff
+parfile $PROTOMSHOME/parameter/solvents.ff
+parfile $PROTOMSHOME/parameter/amber14SB-residues.ff
+parfile $PROTOMSHOME/parameter/gaff16.ff
+parfile vhk-vhq.tem
+protein1 7okh-protein-pms_scoop.pdb
+solute1 7okh-VHK-pdb4amber.pdb
+solute2 7oke-VHQ-xtal-pdb4amber.pdb
+solvent1 water_clr.pdb
+outfolder out_bnd1
+streamheader off
+streamdetail off
+streamwarning warning
+streaminfo info
+streamfatal fatal
+streamresults results
+streamaccept accept
+cutoff 10.0
+feather 0.5
+temperature 25.0
+ranseed 1345829
+boundary solvent
+pdbparams on
+printfe mbar
+dualtopology1 1 2 synctrans syncrot
+softcore1 solute 1
+softcore2 solute 2
+softcoreparams coul 1 delta 0.2 deltacoul 2.5 power 6 soft66
+dlambda 0.001
+lambdare 100000 0.000 0.067 0.133 0.200 0.267 0.333 0.400 0.467 0.533 0.600 0.667 0.733 0.800 0.867 0.933 1.000
+dump 100000 results write results
+dump 100000 results writeinst results_inst
+dump 100000 pdb all solvent=all file=all.pdb standard
+dump 100000 restart write restart
+dump 100000 averages reset
+chunk equilibrate 10000000 solvent=776 protein=217 solute=7 volume=0
+chunk simulate 80000000 solvent=776 protein=217 solute=7 volume=0
---
+++ run1_gcmc.cmd
@@ -0,0 +1,41 @@
+parfile $PROTOMSHOME/parameter/amber14SB.ff
+parfile $PROTOMSHOME/parameter/solvents.ff
+parfile $PROTOMSHOME/parameter/amber14SB-residues.ff
+parfile $PROTOMSHOME/parameter/gaff16.ff
+parfile 7oke-VHQ-xtal-pdb4amber.tem
+protein1 7oke-protein-pdb4amber-pms_scoop.pdb
+solute1 7oke-VHQ-xtal-pdb4amber.pdb
+solvent1 water_clr.pdb
+outfolder out_gcmc1
+streamheader off
+streamdetail off
+streamwarning warning
+streaminfo info
+streamfatal fatal
+streamresults results
+streamaccept accept
+cutoff 10.0
+feather 0.5
+temperature 25.0
+ranseed 492825
+boundary solvent
+pdbparams on
+#  GCMC specific parameters
+gcmc 0
+parfile $PROTOMSHOME/data/gcmc_tip4p.tem
+grand1 gcmc_wat.pdb
+multigcmc 100000 -7.500 -8.000 -8.500 -9.000 -9.500 -10.000 -10.500 -11.000 -11.500 -12.000 -12.500 -13.000 -13.500 -14.000 -14.500 -15.000 -15.500 -16.000 -16.500 -17.000 -17.500 -18.000 -18.500 -19.000 
+originx -80.570
+originy 39.597
+originz 14.340
+x 5.000
+y 6.500
+z 8.000
+#  End of GCMC specific parameters
+dump 100000 results write results
+dump 100000 pdb all solvent=all file=all.pdb standard
+dump 100000 restart write restart
+dump 100000 averages reset
+chunk equilibrate 5000000 solvent=0 protein=0 solute=0 insertion=333 deletion=333 gcsolute=333
+chunk equilibrate 5000000 solvent=390 protein=104 solute=7 insertion=167 deletion=167 gcsolute=167
+chunk simulate 40000000 solvent=390 protein=104 solute=7 insertion=167 deletion=167 gcsolute=167
